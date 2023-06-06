bcae5333fdc839fe9da4995499213b90e2b19be3
Nicolas Torres <1834312+noclat@users.noreply.github.com>
2023-06-06 18:58:15 +0200

    fix:typings (#21)
    * fix: update typings
    
    * update package-lock
    
    * update examples versions
    
    * fix missing handler types
    
    * update package-lock
    
    * update examples versions

--- src/index.d.ts
+++ src/index.d.ts
@@ -5,6 +5,12 @@ export as namespace PlaceKitAutocomplete;
 
 declare function PlaceKitAutocomplete(apiKey?: string, opts?: PKAOptions): PKAClient;
 
+export type PKAState = {
+  dirty: boolean;
+  empty: boolean;
+  freeForm: boolean;
+};
+
 export interface PKAClient {
   readonly input: HTMLInputElement;
   readonly options: PKAOptions;
@@ -14,12 +20,13 @@ export interface PKAClient {
   on(event: 'results', handler?: PKAHandlers['results']): PKAClient;
   on(event: 'pick', handler?: PKAHandlers['pick']): PKAClient;
   on(event: 'error', handler?: PKAHandlers['error']): PKAClient;
+  on(event: 'dirty', handler?: PKAHandlers['dirty']): PKAClient;
   on(event: 'empty', handler?: PKAHandlers['empty']): PKAClient;
   on(event: 'freeForm', handler?: PKAHandlers['freeForm']): PKAClient;
+  on(event: 'state', handler?: PKAHandlers['state']): PKAClient;
   on(event: 'geolocation', handler?: PKAHandlers['geolocation']): PKAClient;
   readonly handlers: Partial<PKAHandlers>;
-  readonly isEmpty: boolean;
-  readonly isFreeForm: boolean;
+  readonly state: PKAState;
   readonly hasGeolocation: boolean;
   requestGeolocation(opts?: Object, cancelUpdate?: boolean): Promise<GeolocationPosition>;
   open(): PKAClient;
@@ -34,8 +41,10 @@ export type PKAHandlers = {
   results: (query: string, results: PKResult[]) => void;
   pick: (value: string, item: PKResult, index: number) => void;
   error: (error: Object) => void;
-  empty: (isEmpty: boolean) => void;
-  freeForm: (isFreeForm: boolean) => void;
+  dirty: (dirty: boolean) => void;
+  empty: (empty: boolean) => void;
+  freeForm: (freeForm: boolean) => void;
+  state: (state: PKAState) => void;
   geolocation: (hasGeolocation: boolean, position?: GeolocationPosition) => void;
 };
 
